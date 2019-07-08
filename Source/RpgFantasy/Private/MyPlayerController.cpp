aaa6f1c6cafdb0ee37d63d99d0bd96700065d1d9
Mateusz kozłowski <kozlowskimateusz151@gmail.com>
2019-07-08 16:43:53 +0200

    Add binding for widgets, add toggle widgets

--- Source/RpgFantasy/Private/MyPlayerController.cpp
+++ Source/RpgFantasy/Private/MyPlayerController.cpp
@@ -1,5 +1,72 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "MyPlayerController.h"
+#include "Blueprint/UserWidget.h"
+#include "Engine.h"
 
+
+AMyPlayerController::AMyPlayerController()
+{
+	bShowCurrentWidget = false;
+}
+
+void AMyPlayerController::BeginPlay()
+{
+
+}
+
+void AMyPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	// Main Menu widget
+	InputComponent->BindAction("MainMenu", IE_Pressed, this, &AMyPlayerController::ToggleMainMenuWidget);
+
+	// Equipment
+	InputComponent->BindAction("Equipment", IE_Pressed, this, &AMyPlayerController::ToggleEquipmentWidget);
+
+	//Skills
+	InputComponent->BindAction("Skills", IE_Pressed, this, &AMyPlayerController::ToggleSkillsWidget);
+
+	//Quests
+	InputComponent->BindAction("Quests", IE_Pressed, this, &AMyPlayerController::ToggleQuestsWidget);
+}
+
+void AMyPlayerController::ToggleWidget(TSubclassOf<UUserWidget> NewWidgetClass, bool bShowMouseCursor, bool bSetPause, bool BInputModeGameOnly)
+{
+	if (CurrentWidget != nullptr)
+	{
+		CurrentWidget->RemoveFromParent();
+		CurrentWidget = nullptr;
+	}
+	if (NewWidgetClass)
+	{
+		CurrentWidget = CreateWidget<UUserWidget>(GetWorld(), NewWidgetClass);
+
+		if (bShowCurrentWidget == false)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Orange, FString("Add Widget"));
+			CurrentWidget->AddToViewport();
+			if (BInputModeGameOnly == true)
+			{
+				SetInputMode(FInputModeGameOnly());
+			}
+			else
+			{
+				SetInputMode(FInputModeGameAndUI());
+			}
+			bShowMouseCursor = bShowMouseCursor;
+			SetPause(bSetPause);
+			bShowCurrentWidget = true;
+		}
+		else
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Orange, FString("remove Widget"));
+			CurrentWidget->RemoveFromParent();
+			SetInputMode(FInputModeGameOnly());
+			bShowMouseCursor = false;
+			SetPause(false);
+			bShowCurrentWidget = false;
+		}
+	}
+}
--- Source/RpgFantasy/Public/MyPlayerController.h
+++ Source/RpgFantasy/Public/MyPlayerController.h
@@ -14,4 +14,50 @@ class RPGFANTASY_API AMyPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+
+public:
+
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable, Category = "Toggle Widget")
+		void ToggleWidget(TSubclassOf<UUserWidget>* NewWidgetClass);
+
+protected:
+
+	AMyPlayerController();
+
+	virtual void SetupInputComponent() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Widgets")
+	TSubclassOf<UUserWidget>* PlayerHud;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Widgets")
+	TSubclassOf<UUserWidget>* PlayerHud;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Widgets")
+	TSubclassOf<UUserWidget>* PlayerHud;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Widgets")
+	TSubclassOf<UUserWidget>* PlayerHud;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Widgets")
+	TSubclassOf<UUserWidget>* PlayerHud;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Widgets")
+	TSubclassOf<UUserWidget>* PlayerHud;
+
+	UPROPERTY()
+	UUserWidget* CurrentWidget;
+
+	bool bShowCurrentWidget = false;
+
+	void ToggleMainMenuWidget();
+
+	void ToggleEquipmentWidget();
+
+	void ToggleSkillsWidget();
+
+	void ToggleMapWidget();
+
+	void ToggleQuestsWidget();
 };
